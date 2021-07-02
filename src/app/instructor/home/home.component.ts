3643eb575640154341a2b991d2e962d9c7a43dcf
ljessicc <ljessicc@gmail.com>
2021-07-02 15:33:28 +0100

    Added a create class button to classes component

--- src/app/instructor/home/home.component.ts
+++ src/app/instructor/home/home.component.ts
@@ -1,26 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import { User } from 'src/app/shared/interfaces/user';
-import { AuthService } from 'src/app/shared/services/auth.service';
+// import { Component, OnInit } from '@angular/core';
+// import { User } from 'src/app/shared/interfaces/user';
+// import { AuthService } from 'src/app/shared/services/auth.service';
 
-@Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
-})
-export class HomeComponent implements OnInit {
+// @Component({
+//   selector: 'app-home',
+//   templateUrl: './home.component.html',
+//   styleUrls: ['./home.component.scss']
+// })
+// export class HomeComponent implements OnInit {
 
-  user!:any;
+//   user!:any;
 
-  constructor(public authService:AuthService) { }
+//   constructor(public authService:AuthService) { }
 
-  ngOnInit(): void {
-    this.getUser()
-  }
+//   ngOnInit(): void {
+//     this.getUser()
+//   }
 
-  getUser() {
-    this.user = this.authService.user;
-    console.log(this.user)
-  }
+//   getUser() {
+//     this.user = this.authService.user;
+//     console.log(this.user)
+//   }
 
 
-}
+// }
