d7fb127481f9b4e9c0aac427c9de3ce3b499899c
ExcelDurant <gnopaexcel@yahoo.fr>
2021-07-02 15:33:14 +0100

    adding firebase service to create a class

---
+++ src/app/shared/interfaces/class.ts
@@ -0,0 +1,7 @@
+export interface Class {
+    id:string;
+    name:string;
+    description:string;
+    createdBy:string;
+    members:[];
+}
--- src/app/shared/services/class.service.ts
+++ src/app/shared/services/class.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import firebase from 'firebase/app';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Class } from '../interfaces/class';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClassService {
 
-  constructor() { }
+  classes: Observable<Class[]>;
+  private classesCollection: AngularFirestoreCollection<any>;
+
+  constructor(private afs: AngularFirestore) {
+    this.classesCollection = afs.collection<Class>('classes');
+    this.classes = this.classesCollection.valueChanges();
+  }
+
+  createClass(classe:Class) {
+    this.classesCollection.doc(classe.id).set(classe, { merge: true });
+  }
+
+  getClass(classe:Class) {
+    const classRef = this.classesCollection.doc(classe.id);
+    return classRef.get().toPromise();
+  }
+
+  deleteClass(classe:Class) {
+    const classRef = this.classesCollection.doc(classe.id);
+    classRef.delete().then(() =>{
+      window.alert("the class has been successfully deleted")
+    })
+  }
+
+  updateClass(classe:Class) {
+    
+  }
 }
